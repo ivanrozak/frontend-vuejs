3ec78475ff5a0d04605545ab6bb695c403ed61ef
ivanrozak <ivanrozack@gmail.com>
2020-12-16 21:12:20 +0700

    adding patch, get, delete product

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -4,17 +4,34 @@
       <img alt="Vue logo" src="../assets/logo.png" />
       <Navbar />
       <h1>This is Page Product</h1>
+      <b-alert :show="alert">{{ isMsg }}</b-alert>
       <form>
-        <input type="text" placeholder="Product Name ..." />
+        <input
+          type="text"
+          v-model="form.product_name"
+          placeholder="Product Name ..."
+        />
         <br />
-        <input type="text" placeholder="Product Price ..." />
+        <input
+          type="text"
+          v-model="form.product_price"
+          placeholder="Product Price ..."
+        />
         <br />
-        <input type="text" placeholder="Category Id ..." />
+        <input
+          type="text"
+          v-model="form.category_id"
+          placeholder="Category Id ..."
+        />
         <br />
-        <input type="text" placeholder="Product Status ..." />
+        <input
+          type="text"
+          v-model="form.product_status"
+          placeholder="Product Status ..."
+        />
         <br />
-        <button type="submit">Save</button>
-        <button type="button">Update</button>
+        <button type="button" @click="postProduct()">Save</button>
+        <button type="button" @click="patchProduct()">Update</button>
       </form>
       <hr />
       <b-container class="bv-example-row">
@@ -39,14 +56,24 @@
               <b-card-text> Rp. {{ item.product_price }} </b-card-text>
 
               <b-button href="#" variant="primary">Add to Cart</b-button>
-              <b-button href="#" variant="success">Update</b-button>
-              <b-button href="#" variant="danger">Delete</b-button>
+              <b-button href="#" variant="success" @click="setProduct(item)"
+                >Update</b-button
+              >
+              <b-button variant="danger" @click="deleteProduct(item.product_id)"
+                >Delete</b-button
+              >
             </b-card>
           </b-col>
           <b-col xl="3" lg="4" md="6" sm="12">2 of 3</b-col>
           <b-col xl="3" lg="4" md="6" sm="12">3 of 3</b-col>
           <b-col xl="3" lg="4" md="6" sm="12">4 of 3</b-col>
         </b-row>
+        <b-pagination
+          v-model="currentPage"
+          :total-rows="rows"
+          :per-page="limit"
+          @change="handlePageChange"
+        ></b-pagination>
       </b-container>
     </div>
   </div>
@@ -63,6 +90,11 @@ export default {
   components: {
     Navbar
   },
+  computed: {
+    rows() {
+      return this.totalRows
+    }
+  },
   data() {
     return {
       products: [],
@@ -71,7 +103,14 @@ export default {
         category_id: '',
         product_price: '',
         product_status: ''
-      }
+      },
+      alert: false,
+      isMsg: '',
+      product_id: '',
+      currentPage: 1,
+      totalRows: null,
+      limit: 8,
+      page: 1
     }
   },
   created() {
@@ -80,14 +119,54 @@ export default {
   methods: {
     getProduct() {
       axios
-        .get('http://localhost:3000/product?page=1&limit=4')
+        .get(
+          `http://localhost:3000/product?page=${this.page}&limit=${this.limit}`
+        )
         .then(response => {
           console.log(response)
+          this.totalRows = response.data.pagination.totalData
           this.products = response.data.data
         })
         .catch(error => {
           console.log(error)
         })
+    },
+    postProduct() {
+      console.log(this.form)
+      axios
+        .post('http://localhost:3000/product', this.form)
+        .then(response => {
+          console.log(response)
+          this.alert = true
+          this.isMsg = response.data.msg
+          this.getProduct()
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    deleteProduct(product_id) {
+      console.log(product_id)
+    },
+    setProduct(data) {
+      console.log(data)
+      // this.form = {
+      //   product_name: 'data.product_name',
+      //   category_id: 'data.category_id',
+      //   product_price: 'data.product_price',
+      //   product_status: 'data.product_status'
+      // }
+      this.form = data //cara simplenya
+      this.product_id = data.product_id
+    },
+    patchProduct() {
+      console.log(this.product_id)
+      console.log(this.form)
+    },
+    handlePageChange(numberPage) {
+      console.log(numberPage)
+      this.page = numberPage
+      this.getProduct()
     }
   }
 }
